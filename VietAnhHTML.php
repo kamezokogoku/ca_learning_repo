770bec1c14db610c069d5c144f9697b5a0843e8e
kamezokogoku <81956061+kamezokogoku@users.noreply.github.com>
2021-10-03 18:38:24 +0300

    Add files via upload

---
+++ VietAnhHTML.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        $a = "Hello";
+        $b ="phptest";
+        $c = "cityads";
+        ?>
+    <meta charset="UTF-8">
+    <title<?php echo $a; ?></title>
+</head>
+<body>
+<h1><?php echo $b;?></h1>
+<h2><?php echo $c;?></h2>
+<h3><?php echo date(d.D.M.Y);?></h3>
+</body>
